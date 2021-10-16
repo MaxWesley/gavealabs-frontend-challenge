70ddf49fafb75f4c7fa84d0b73965301ffadb274
MaxWesley <mx_wesley@hotmail.com>
2021-10-16 04:51:17 -0300

    feat: add login page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Onboarding } from './pages/Onboarding';
+import { Login } from './pages/Login';
 
 function Routes() {
     return(
         <Router>
             <Switch>
-                <Route path="/" component={Onboarding} />
+                <Route path="/" component={Onboarding} exact />
+                <Route path="/login" component={Login} exact />
             </Switch>
         </Router>
     );
