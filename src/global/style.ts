6f7a167528ec6f7e8f9b9824e7ad524035f5ab95
MaxWesley <mx_wesley@hotmail.com>
2021-10-16 01:41:01 -0300

    feat: create/adding GlobalStyle and project fonts

---
+++ src/global/style.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Karla:wght@400;700&display=swap');
+    
+    * {
+        padding: 0;
+        margin: 0;
+        border: 0;
+        
+        box-sizing: border-box;
+    }
+    
+    ul {
+        list-style: none;
+    }
+
+    button,
+    input {
+        outline: none;
+    }
+`;
