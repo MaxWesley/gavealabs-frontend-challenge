94fe1b3af5f75640ac15e493b95a7624a5d81864
MaxWesley <mx_wesley@hotmail.com>
2021-10-16 04:48:13 -0300

    feat: create page Login

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,44 @@
+import { TextInput } from "../../components/TextInput";
+import { Container, CreateAccountText, Form, FormText } from "./styles";
+
+import emailIconPNG from '../../assets/images/icons/email.png';
+import lockerIconPNG from '../../assets/images/icons/locker.png';
+
+import { Button } from "../../components/Button";
+import { Link } from "react-router-dom";
+
+function Login() {
+    return (
+        <Container>
+            <Form>
+                <FormText>
+                    <span>Olá!</span>
+                    Seja bem-vindo.
+                </FormText>
+                <main>
+                    <TextInput
+                        label="Email"
+                        icon={<img src={emailIconPNG} />}
+                        inputType="email"
+                    />
+                    <TextInput
+                        label="Senha"
+                        icon={<img src={lockerIconPNG} />}
+                        isSecurity={true}
+                    />
+                </main>
+                <Button
+                    buttonStyle="confirm"
+                    content="Entrar"
+                    buttonType="submit"
+                    action={() => alert('clicou')}
+                />
+            </Form>
+            <CreateAccountText>
+                Não tem uma conta? <Link to="/sign-up">Criar conta</Link>
+            </CreateAccountText>
+        </Container>
+    );
+}
+
+export { Login };
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+
+    padding: 21px 38px;
+
+    background-color: #FFFFFF;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+export const FormText = styled.h1`
+    font-family: 'Inter', sans-serif;
+    font-weight: 600;
+    font-size: 36px;
+
+    line-height: 42px;
+
+    color: #092A34;
+
+    margin-top: 125px;
+
+    span {
+        display: block;
+        color: #FF6900;
+    }
+`;
+
+export const Form = styled.form`
+    main {
+        margin-top: 31px;
+        margin-bottom: 30px;
+    }
+`;
+
+export const CreateAccountText = styled.p`
+    font-family: 'Karla', sans-serif;
+    font-weight: 400;
+    font-size: 15px;
+
+    text-align: center;
+
+    color: #748C94;
+
+    a {
+        font-weight: 700;
+        color: #FF6900;
+    }
+`;
