5440d95474409f586f2070ecc7791b2dc147837e
MaxWesley <mx_wesley@hotmail.com>
2021-10-19 00:25:58 -0300

    feat: create datalist commodities

---
+++ src/services/datas.ts
@@ -0,0 +1,65 @@
+import avatar01PNG from '../assets/images/avatar01.png';
+import avatar02PNG from '../assets/images/avatar02.png';
+import avatar03PNG from '../assets/images/avatar03.png';
+import avatar04PNG from '../assets/images/avatar04.png';
+import avatar05PNG from '../assets/images/avatar05.png';
+import avatar06PNG from '../assets/images/avatar06.png';
+
+export const datas = [
+    {
+        "img": avatar01PNG,
+        "part": "AgAmerica",
+        "pag": "Fev/21",
+        "ton": "2.000",
+        "ammount": "164,50",
+        "tagColor": "green"
+    },
+    {
+        "img": avatar02PNG,
+        "part": "TrFood",
+        "pag": "Fev/21",
+        "ton": "2.000",
+        "ammount": "164,50",
+        "tagColor": "green"
+    },
+    {
+        "img": avatar03PNG,
+        "part": "GOSC",
+        "pag": "Fev/21",
+        "ton": "2.000",
+        "ammount": "164,50",
+        "tagColor": "red"
+    },
+    {
+        "img": avatar04PNG,
+        "part": "AgCerr",
+        "pag": "Fev/21",
+        "ton": "2.000",
+        "ammount": "164,50",
+        "tagColor": "red"
+    },
+    {
+        "img": avatar05PNG,
+        "part": "Agro1001",
+        "pag": "Fev/21",
+        "ton": "2.000",
+        "ammount": "164,50",
+        "tagColor": "green"
+    },
+    {
+        "img": avatar06PNG,
+        "part": "AgAmerica",
+        "pag": "Fev/21",
+        "ton": "2.000",
+        "ammount": "164,50",
+        "tagColor": "green"
+    },
+    {
+        "img": avatar02PNG,
+        "part": "TrFood",
+        "pag": "Fev/21",
+        "ton": "2.000",
+        "ammount": "145,50",
+        "tagColor": "green"
+    }
+]
